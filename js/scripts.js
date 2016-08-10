21d697c9aa767f33e494d14987599d9d9b2a3093
Sara Jensen and Zachary Matthewstearn <zachary@>
2016-08-10 10:23:02 -0700

    minorly tweak js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $("#name-input").click(function(event){
   $(this).val("");
-    event.preventDefault();
+  event.preventDefault();
 });
 
 $("#name-input").keyup(function(event){
   if(event.keyCode === 13){
     $("#name").text($(this).val());
     $("#letter").show();
-    event.preventDefault();
   }
+  event.preventDefault();
 });
